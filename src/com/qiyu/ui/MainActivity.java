eccd05af44c2d85f88ddece903a05521cf07c39f
Administrator <Administrator@PC-20150714XFSX>
2016-03-07 10:53:57 +0800

    1

---
+++ src/com/qiyu/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.qiyu.ui;
+
+import com.qiyu.droidheard.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	
+		setContentView(R.layout.main_layout);
+	
+	}
+	
+}

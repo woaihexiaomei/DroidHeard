c2ac93d5503ea61b2e8ef9e221ea8125a3c07a71
wan <wan@192.168.0.105>
2016-03-08 08:11:02 +0800

    1

--- src/com/qiyu/ui/MainActivity.java
+++ src/com/qiyu/ui/MainActivity.java
@@ -1,12 +1,18 @@
 package com.qiyu.ui;
 
+import com.baidu.android.pushservice.PushConstants;
+import com.baidu.android.pushservice.PushManager;
 import com.qiyu.droidheard.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity{
 
+	Button btn;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,25 @@ public class MainActivity extends Activity{
 		setContentView(R.layout.main_layout);
 		
 		
+		btn = (Button) findViewById(R.id.btn);
+		
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+		
+				PushManager.startWork(getApplicationContext(), PushConstants.LOGIN_TYPE_API_KEY, "{HFno9tx9U6EOEThmuvubRvVj}");		
+				
+			}
+		});
+		
+		 
 		
 		
 		
 	}
+	
+	
 	
 }
--- src/com/qiyu/util/receive/RecordReceive.java
+++ src/com/qiyu/util/receive/RecordReceive.java
@@ -1,19 +1,70 @@
 package com.qiyu.util.receive;
 
+import java.util.List;
+
+import com.baidu.android.pushservice.PushMessageReceiver;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class RecordReceive extends BroadcastReceiver{
+public class RecordReceive extends PushMessageReceiver{
+
 
 	@Override
-	public void onReceive(Context arg0, Intent arg1) {
+	public void onBind(Context arg0, int arg1, String arg2, String arg3,
+			String arg4, String arg5) {
 		// TODO Auto-generated method stub
 		
-		String action = arg1.getAction();
+	}
+
+	@Override
+	public void onDelTags(Context arg0, int arg1, List<String> arg2,
+			List<String> arg3, String arg4) {
+		// TODO Auto-generated method stub
 		
-		Log.e("innog", "action"+action);
+	}
+
+	@Override
+	public void onListTags(Context arg0, int arg1, List<String> arg2,
+			String arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessage(Context arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+	
+		Log.e("innog", "innog------------"+arg1);
+		
+	}
+
+	@Override
+	public void onNotificationArrived(Context arg0, String arg1, String arg2,
+			String arg3) {
+		// TODO Auto-generated method stub
+		Log.e("innog", "innog------------"+arg1);
+	}
+
+	@Override
+	public void onNotificationClicked(Context arg0, String arg1, String arg2,
+			String arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSetTags(Context arg0, int arg1, List<String> arg2,
+			List<String> arg3, String arg4) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUnbind(Context arg0, int arg1, String arg2) {
+		// TODO Auto-generated method stub
 		
 	}
 
